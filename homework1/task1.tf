d6a975eb0d823d3d157a39e6b1dc39647754a012
nurisapa <saparbekovanuriza@ymail.com>
2024-04-04 17:01:25 +0000

    added homework files

---
+++ homework1/task1.tf
@@ -0,0 +1,84 @@
+resource "aws_iam_user" "lb" {
+  name = "jenny"
+}
+
+resource "aws_iam_user" "lb2" {
+  name = "rose"
+}
+
+resource "aws_iam_user" "lb3" {
+  name = "lisa"
+}
+
+resource "aws_iam_user" "lb4" {
+  name = "jisoo"
+}
+
+resource "aws_iam_user" "lb5" {
+  name = "jihyo"
+}
+
+resource "aws_iam_user" "lb6" {
+  name = "sana"
+}
+
+resource "aws_iam_user" "lb7" {
+  name = "momo"
+}
+
+resource "aws_iam_user" "lb8" {
+  name = "dahyun"
+}
+
+
+
+resource "aws_iam_group" "blackpink" {
+  name = "blackpink"
+}
+
+resource "aws_iam_group_membership" "blackpink" {
+  name = "tf-testing-group-membership"
+
+  users = [
+    aws_iam_user.lb.name,
+    aws_iam_user.lb2.name,
+    aws_iam_user.lb3.name,
+    aws_iam_user.lb4.name,
+    aws_iam_user.lb5.name,
+    aws_iam_user.lb9.name
+
+  ]
+
+  group = aws_iam_group.blackpink.name
+}
+
+resource "aws_iam_group" "twice" {
+  name = "twice"
+}
+resource "aws_iam_group_membership" "twice" {
+  name = "tf-testing-group-membership"
+
+  users = [
+    aws_iam_user.lb5.name,
+    aws_iam_user.lb6.name,
+    aws_iam_user.lb7.name,
+    aws_iam_user.lb8.name,
+    aws_iam_user.lb10.name
+
+
+  ]
+
+  group = aws_iam_group.twice.name
+}
+
+resource "aws_iam_user" "lb9" {
+  name = "miyeon"
+}
+
+#terraform import aws_iam_user.lb9 miyeon
+
+resource "aws_iam_user" "lb10" {
+  name = "mina"
+}
+
+#terraform import aws_iam_user.lb10 mina
