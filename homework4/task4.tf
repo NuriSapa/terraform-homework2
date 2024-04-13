25c6e08448ef8a24e94fa11b8e4cf8f05a0e9470
nurisapa <saparbekovanuriza@ymail.com>
2024-04-13 14:27:57 +0000

    added

---
+++ homework4/task4.tf
@@ -0,0 +1,30 @@
+provider aws {
+    region = var.region
+
+}
+
+resource "aws_instance" "web" {
+  ami = var.ami_id
+
+  #ami uniq for each region need to pay attention
+  instance_type = var.type
+
+}
+
+variable ami_id  {
+  description = "Provide ami id"
+  default = "ami-0900fe555666598a2"
+  type = string 
+}
+
+variable type  {
+  description = "Provide instance type"
+  default = "t2.micro"
+  type = string
+}
+
+variable region  {
+  description = "Provide region"
+  default = "us-east-2"
+  type = string
+}
